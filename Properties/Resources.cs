2f622d13bded1a7b43deb3f311754a2871ac6894
x1234xx <40677860+x1234xx@users.noreply.github.com>
2021-06-07 02:24:53 +0200

    Add files via upload

---
+++ Properties/Resources.cs
@@ -0,0 +1,44 @@
+﻿
+using System.CodeDom.Compiler;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Globalization;
+using System.Resources;
+using System.Runtime.CompilerServices;
+
+namespace BankRev.Properties
+{
+  [CompilerGenerated]
+  [DebuggerNonUserCode]
+  [GeneratedCode("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
+  internal class Resources
+  {
+    private static ResourceManager resourceMan;
+    private static CultureInfo resourceCulture;
+
+    internal Resources()
+    {
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static ResourceManager ResourceManager
+    {
+      get
+      {
+        if (object.ReferenceEquals((object) BankRev.Properties.Resources.resourceMan, (object) null))
+          BankRev.Properties.Resources.resourceMan = new ResourceManager("BankRev.Properties.Resources", typeof (BankRev.Properties.Resources).Assembly);
+        return BankRev.Properties.Resources.resourceMan;
+      }
+    }
+
+    [EditorBrowsable(EditorBrowsableState.Advanced)]
+    internal static CultureInfo Culture
+    {
+      get => BankRev.Properties.Resources.resourceCulture;
+      set => BankRev.Properties.Resources.resourceCulture = value;
+    }
+
+    internal static Bitmap folder => (Bitmap) BankRev.Properties.Resources.ResourceManager.GetObject(nameof (folder), BankRev.Properties.Resources.resourceCulture);
+  }
+}
---
+++ Properties/Settings.cs
@@ -0,0 +1,23 @@
+﻿
+using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace BankRev.Properties
+{
+  [GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "12.0.0.0")]
+  [CompilerGenerated]
+  internal sealed class Settings : ApplicationSettingsBase
+  {
+    private static Settings defaultInstance = (Settings) SettingsBase.Synchronized((SettingsBase) new Settings());
+
+    public static Settings Default
+    {
+      get
+      {
+        Settings defaultInstance = Settings.defaultInstance;
+        return defaultInstance;
+      }
+    }
+  }
+}
